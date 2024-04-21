d72fd97b6247f87a93e6bcdaa4fc80e1a0b61ef9
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-22 00:24:03 +0600

    user data retrieved from session token

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,31 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user?.email && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome{" "}
+            <span className="text-green-400 italic">{session?.user?.name}</span>
+          </h1>
+          <h1 className="text-2xl text-center mt-10">
+            Logged in with{" "}
+            <span className="text-green-400">{session?.user?.email}</span>
+          </h1>
+          <Image
+            src={session?.user?.image} 
+            alt="image"
+            width={150}
+            height={150}
+            className="mx-auto rounded-full"
+          />
+        </>
+      )}
     </div>
   );
 };
